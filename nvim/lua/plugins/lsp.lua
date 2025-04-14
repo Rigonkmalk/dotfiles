590b106813011ffefa2ee188193f68507cd0051c
Rigonkmalk <rigonkmalk@gmail.com>
2025-04-14 17:04:14 +0200

    feat: update & modified lsp
    delete old install from lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -158,13 +158,4 @@ return {
             })
         end
     },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        config = function()
-            local mason_lspconfig = require 'mason-lspconfig'
-            mason_lspconfig.setup {
-                ensure_installed = languages
-            }
-        end
-    },
 }
